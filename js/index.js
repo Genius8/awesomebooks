f1ec8aa469050eb767c8e24d084d79b103c05f85
Maclenn77 <maclenn77@gmail.com>
2021-11-22 16:18:35 -0600

    Create Function for book ids

--- js/index.js
+++ js/index.js
@@ -3,9 +3,19 @@ function Book(title, author) {
     this.author = author;
 }
 
+var bookIdMap = new WeakMap, bookCount = 0;
+
+function bookID(book){
+    if (!bookIdMap.has(book)) bookIdMap.set(book, ++bookCount);
+    return bookIdMap.get(book);
+}
+
 var example = new Book('Lorem Ipsum', 'Tester');
 var example2 = new Book('Second book', 'Testero Testyy');
 
 let books = [{}];
+
+
+
 
 books.push(example)
